1bca08b041b73edbcb67ac5a7aca6dcd486bb06f
Vinicius Minervino Gering <vinicius.gering@hotmail.com>
2025-05-23 01:14:13 -0300

    insert timer to get treatment of orders

--- OrderService/Worker.cs
+++ OrderService/Worker.cs
@@ -1,8 +1,13 @@
+using Application.Services;
 using Domain.Dtos;
 using Domain.Interfaces;
 using Infrasctructure.Queues;
+using Microsoft.Extensions.Logging;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using System;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
 using System.Text;
 using System.Text.Json;
 
@@ -14,6 +19,7 @@ namespace OrderService
         private readonly IOrderService _orderService;
         private readonly IServiceScopeFactory _scopeFactory;
         private readonly IModel _channel;
+        private readonly string _queueName = "orders";
 
         public Worker(IServiceScopeFactory scopeFactory, ILogger<Worker> logger, IOrderService orderService, IModel channel)
         {
@@ -25,27 +31,35 @@ namespace OrderService
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            try
+            _channel.QueueDeclare(queue: _queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += async (model, ea) =>
             {
-                var consumer = new AsyncEventingBasicConsumer(_channel);
-                consumer.Received += async (sender, eventArgs) =>
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+
+                _logger.LogInformation($"Received Message: {message}");
+
+                var stopwatch = Stopwatch.StartNew();
+
+                try
                 {
-                    var body = eventArgs.Body.ToArray();
-                    var message = Encoding.UTF8.GetString(body);
-
-                    _logger.LogInformation("Received message: {message}", message);
                     await ProcessMessage(message);
-                };
+                    _logger.LogInformation($"Process concluded in {stopwatch.ElapsedMilliseconds} ms");
+                    _channel.BasicAck(ea.DeliveryTag, false); // Confirma a mensagem processada
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError($"Error to Process Order ({stopwatch.ElapsedMilliseconds} ms): {ex.Message}");
+                }
+            };
 
-                _channel.BasicConsume(queue: "orders", autoAck: true, consumer: consumer);
+            _channel.BasicConsume(queue: _queueName, autoAck: false, consumer: consumer);
 
-                await Task.CompletedTask;
-
-            }
-            catch (Exception ex)
+            while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogError("Error to try process message: {ex}", ex.Message);
-                throw;
+                await Task.Delay(1000, stoppingToken); // Mantém Worker ativo
             }
         }
 
@@ -57,6 +71,11 @@ namespace OrderService
                 if (orderDto != null)
                 {
                     await _orderService.ProcessOrder(orderDto);
+                    _logger.LogInformation($"Order Processed: {orderDto.Id}");
+                }
+                else
+                {
+                    _logger.LogWarning("Error in Desserealize of message!.");
                 }
             }
         }
